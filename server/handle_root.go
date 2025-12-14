0a86bab8317e87a79935110e5ad34eda49d65919
scanash00 <scan@scanash.com>
2025-12-13 17:36:33 -0900

    root message for open fork

--- server/handle_root.go
+++ server/handle_root.go
@@ -5,36 +5,28 @@ import "github.com/labstack/echo/v4"
 func (s *Server) handleRoot(e echo.Context) error {
 	return e.String(200, `
 
-                                 ....-*%%%##### 
-                     .%#+++****#%%%%%%%%%#+:....
-            .%+++**++++*%%%%.....               
-     .%+++*****#%%%%#.. %#%...                  
- ***+*****%%%%%...       =..                    
- *****%%%%..            +=++..                  
- %%%%%...             .+----==++.               
-                     .-::----===++              
-                   .=-:.------==+++             
-                   +-:::-:----===++..           
-                   =-::-----:-==+++-.           
-                  .==*=------==++++.            
-                  +-:--=++===*=--++.            
-                  +:::--:=++=----=+..           
-                  *::::---=+#----=+.            
-                  =::::----=+#---=+..           
-                  .::::----==+=--=+..           
-                  .-::-----==++=-=+..           
-                   -::-----==++===+..           
-                   =::-----==++==++             
-                   +::----:==++=+++             
-                   :-:----:==+++++.             
-                   .=:=----=+++++.              
-                    +=-=====+++..               
-                     =====++.                   
-                      =++...                    
+    _\/_
+     /\
+     /\
+    /  \
+    /~~\o
+   /o   \
+  /~~*~~~\
+ o/    o \
+ /~~~~~~~~\~`
+/__*_______\
+     ||
+   \====/
+    \__/                    
 
 
-This is an AT Protocol Personal Data Server (aka, an atproto PDS)
+This is an AT Protocol Personal Data Server (aka, an atproto PDS) hosted by Scan
 
-Code: https://github.com/haileyok/cocoon
+Feel free to join or migrate by using https://pdsmoover.com or goat-cli
+
+Donate: https://ko-fi.com/scan
+Follow Scan: https://bsky.app/profile/scanash.com
+Code: https://github.com/scanash00/cocoon
+
 Version: `+s.config.Version+"\n")
 }
