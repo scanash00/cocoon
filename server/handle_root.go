7175e59a344ebaa38c6c714b881a927f5db298c9
scanash00 <scan@scanash.com>
2026-01-02 15:57:11 -0900

    implement styled root page

--- server/handle_root.go
+++ server/handle_root.go
@@ -3,38 +3,7 @@ package server
 import "github.com/labstack/echo/v4"
 
 func (s *Server) handleRoot(e echo.Context) error {
-	return e.String(200, `
-
-                                 ....-*%%%##### 
-                     .%#+++****#%%%%%%%%%#+:....
-            .%+++**++++*%%%%.....               
-     .%+++*****#%%%%#.. %#%...                  
- ***+*****%%%%%...       =..                    
- *****%%%%..            +=++..                  
- %%%%%...             .+----==++.               
-                     .-::----===++              
-                   .=-:.------==+++             
-                   +-:::-:----===++..           
-                   =-::-----:-==+++-.           
-                  .==*=------==++++.            
-                  +-:--=++===*=--++.            
-                  +:::--:=++=----=+..           
-                  *::::---=+#----=+.            
-                  =::::----=+#---=+..           
-                  .::::----==+=--=+..           
-                  .-::-----==++=-=+..           
-                   -::-----==++===+..           
-                   =::-----==++==++             
-                   +::----:==++=+++             
-                   :-:----:==+++++.             
-                   .=:=----=+++++.              
-                    +=-=====+++..               
-                     =====++.                   
-                      =++...                    
-
-
-This is an AT Protocol Personal Data Server (aka, an atproto PDS)
-
-Code: https://github.com/haileyok/cocoon
-Version: `+s.config.Version+"\n")
+	return e.Render(200, "index", map[string]interface{}{
+		"Version": s.config.Version,
+	})
 }
